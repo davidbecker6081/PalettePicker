01be90ff1fba156a98d0df0484b8c6fdd2d5aa28
David Becker <david@beckersite.com>
2017-10-02 15:53:06 -0600

    add populateColorObj function to make a palette object

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,34 @@
-const randoPalettes = $('.gen-color');
+// const randoPalettes = $('.gen-color');
+// const generateBtn = $('.generate-btn');
 
-const generateHexValues = (num) => {
-  let hexValues = []
-  for(let i = 0; i < num; i++) {
-    hexValues.push('#' + (Math.random() * 0xFFFFFF << 0).toString(16))
+$('.generate-btn').on('click', () => {
+  populateColorObj(generateHexValues(5));
+});
+
+const generateHexValues = num => {
+	let hexValues = [];
+	for (let i = 0; i < num; i++) {
+		hexValues.push('#' + ((Math.random() * 0xffffff) << 0).toString(16));
+	}
+	return hexValues;
+};
+
+// console.log(generateHexValues(5));
+
+const populateColorObj = hexArray => {
+	const palette = {
+    color1: '',
+    color2: '',
+    color3: '',
+    color4: '',
+    color5: '',
   }
-  return hexValues
-}
 
-console.log(generateHexValues(5))
+  Object.keys(palette).map((key, i) => {
+    palette[key] = hexArray[i]
+  })
+};
+
+const populateColorSwatch = (paletteObj) => {
+  const randoPalettes = $('.gen-color');
+}
