dee842a9e3db55ad4d60d7ccebb06c94f2155c68
David Becker <david@beckersite.com>
2017-10-03 15:47:13 -0600

    fix palette objects and swatches

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,47 @@
 // const randoPalettes = $('.gen-color');
 // const generateBtn = $('.generate-btn');
 
+// array of swatch objects
+// [
+// {color: '', locked: false},
+// {color: '', locked: false},
+// {color: '', locked: false},
+// {color: '', locked: false},
+// {color: '', locked: false},
+// ]
+
+const palette = [];
+
 $('.generate-btn').on('click', () => {
 	populateColorSwatch(populateColorObj(generateHexValues(5)));
 });
+
+$('.lock-img').on('click', e => {
+	// $(e.target).toggleClass('lock-img-locked')
+	toggleLock(e);
+	console.log('lock', palette);
+});
+
+const toggleLock = e => {
+	console.log(
+		$(e.target)
+			.parent('article')
+			.children('p')
+			.text(),
+	);
+	const hexValue = $(e.target)
+		.parent('article')
+		.children('p')
+		.text();
+
+	$(e.target).toggleClass('lock-img-locked');
+
+	palette.forEach(swatch => {
+		if (swatch.color === hexValue) {
+			swatch.locked = !swatch.locked;
+		}
+	});
+};
 
 const generateHexValues = num => {
 	let hexValues = [];
@@ -13,31 +51,171 @@ const generateHexValues = num => {
 	return hexValues;
 };
 
-// console.log(generateHexValues(5));
-
 const populateColorObj = hexArray => {
-	const palette = {
-		color1: '',
-		color2: '',
-		color3: '',
-		color4: '',
-		color5: '',
-	};
+	for (let i = 0; i < 5; i++) {
+		let swatch = {
+			color: '',
+			locked: false,
+		};
 
-	Object.keys(palette).map((key, i) => {
-		palette[key] = hexArray[i];
-	});
+		if (palette.length === 5 && palette[i].locked) {
+			console.log(i);
+			swatch.color = palette[i].color;
+			swatch.locked = true;
+			palette.splice(i, 1, swatch);
+		} else if (palette.length === 5 && !palette[i].locked) {
+			swatch.color = hexArray[i];
+			palette.splice(i, 1, swatch);
+		} else {
+			swatch.color = hexArray[i];
+			palette.push(swatch);
+		}
 
-	return palette;
+		console.log(swatch);
+	}
 };
 
-const populateColorSwatch = paletteObj => {
-	const paletteKeys = Object.keys(paletteObj);
-
-	$('.gen-color').each((i, palette) => {
-    let color = paletteKeys[i]
-		$(palette).css('background-color', paletteObj[color]);
-    $(palette).children('p').text(paletteObj[color])
+const populateColorSwatch = () => {
+	// const paletteKeys = Object.keys(paletteObj);
+	console.log(palette);
+	$('.gen-color').each((i, swatch) => {
+		let color = palette[i].color;
+		$(swatch).css('background-color', color);
+		$(swatch)
+			.children('p')
+			.text(color);
 	});
+};
 
+const appendProjects = projects => {
+  const projects = mockRetrieve()
+  const projectKeys = Object.keys(projects)
+  const projectDisplay = projectKeys.map((projectKey, i) => `<article class="project-container" id="projectContainer">
+    <h3>${projectKey}</h3>
+    <div class="palette-container" id="paletteContainer">
+      <h4>Palette Name</h4>
+      ${projects[projectKey].map()}
+      <div class="palette-colors-container" id="paletteColors">
+        <div class="palette-color" id="paletteColor"></div>
+        <div class="palette-color" id="paletteColor"></div>
+        <div class="palette-color" id="paletteColor"></div>
+        <div class="palette-color" id="paletteColor"></div>
+        <div class="palette-color" id="paletteColor"></div>
+      </div>
+      <button class="delete-btn" id="deleteBtn">Delete</button>
+    </div>
+  </article>`)
+
+  $('.project.display').append()
+};
+
+const mockRetrieve = () => {
+	return {
+		Project1: {
+			palettes: [
+				{
+					paletteName: 'paletteFun',
+					colors: ['#FFFFFF', '#000000', '#FFFFFF', '#000000', '#FFFFFF'],
+					foreignID: '1',
+				},
+				{
+					paletteName: 'paletteNotFun',
+					colors: ['#000000', '#000000', '#000000', '#000000', '#000000'],
+					foreignID: '1',
+				},
+			],
+		},
+    Project2: {
+      palettes: [
+        {
+    			paletteName: 'paletteOther',
+    			colors: ['#F9D0F0', '#D6C400', '#445566', '#A1C1D1', '#B9C3D6'],
+    			foreignID: '1',
+    		}
+      ]
+    }
+	};
+};
+
+
+const retrieveProjects = () => {
+	// fetch('/api/projects')
+	// .then(response => {
+	//   if (response.status !== 200) {
+	//     return false
+	//   }
+	//   return response
+	// })
+	// .then(response => response.json())
+	// .then(parsedResponse => appendProjects(parsedResponse))
+};
+
+// fetch('/api/users', {
+// 			method: 'POST',
+// 			body: JSON.stringify(user),
+// 			headers: {
+// 				'Content-Type': 'application/json'
+// 			}
+// 		})
+// 			.then(response => {
+// 				if (response.status !== 200) {
+// 					dispatch(loginHasErred(true));
+// 				} else {
+// 					return response;
+// 				}
+// 			})
+
+const Projects = () => {
+	return {
+		'1': { projectName: 'Project1' },
+		'2': { projectName: 'Project2' },
+	};
+};
+
+const Palettes = () => {
+	return {
+		'1': {
+			paletteName: 'paletteFun',
+			colors: ['#FFFFFF', '#000000', '#FFFFFF', '#000000', '#FFFFFF'],
+			foreignID: '1',
+		},
+		'2': {
+			paletteName: 'paletteNotFun',
+			colors: ['#000000', '#000000', '#000000', '#000000', '#000000'],
+			foreignID: '1',
+		},
+		'3': {
+			paletteName: 'paletteOther',
+			colors: ['#F9D0F0', '#D6C400', '#445566', '#A1C1D1', '#B9C3D6'],
+			foreignID: '1',
+		},
+	};
+};
+
+const mockRetrieve = () => {
+	return {
+		Project1: {
+			palettes: [
+				{
+					paletteName: 'paletteFun',
+					colors: ['#FFFFFF', '#000000', '#FFFFFF', '#000000', '#FFFFFF'],
+					foreignID: '1',
+				},
+				{
+					paletteName: 'paletteNotFun',
+					colors: ['#000000', '#000000', '#000000', '#000000', '#000000'],
+					foreignID: '1',
+				},
+			],
+		},
+    Project2: {
+      palettes: [
+        {
+    			paletteName: 'paletteOther',
+    			colors: ['#F9D0F0', '#D6C400', '#445566', '#A1C1D1', '#B9C3D6'],
+    			foreignID: '1',
+    		}
+      ]
+    }
+	};
 };
