a556b447ff25322493d36dbf39483720817b7f88
David Becker <david@beckersite.com>
2017-10-03 19:53:11 -0600

    Add delete method for endpoint palette

--- server.js
+++ server.js
@@ -57,15 +57,31 @@ app.get('/api/projects', (request, response) => {
     })
 })
 
-// app.get('/api/v1/papers', (request, response) => {
-//   database('papers').select()
-//     .then((papers) => {
-//       response.status(200).json(papers);
-//     })
-//     .catch((error) => {
-//       response.status(500).json({ error });
-//     });
-// });
+app.get('/api/palettes', (request, response) => {
+  database('palettes').select()
+    .then(palettes => {
+      response.status(200).json(palettes)
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
+
+app.delete('/api/palettes/:paletteName', (request, response) => {
+  const { projectId } = request.body;
+  const { paletteName } = request.params;
+
+  database('palettes')
+    .where('palette_name', paletteName)
+    .where('project_id', projectId)
+    .del()
+    .then(() => {
+      response.status(200).json('success')
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
 
 app.listen(app.get('port'), () => {
 	console.log(`${app.locals.title} is running on ${app.get('port')}.`);
