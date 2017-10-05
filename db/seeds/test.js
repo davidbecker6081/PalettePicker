7a6312ca90ebba4101b83fbd9907220e62a09e39
David Becker <david@beckersite.com>
2017-10-05 14:53:41 -0600

    Finish seed file for testing

--- db/seeds/test.js
+++ db/seeds/test.js
@@ -33,90 +33,46 @@ let projectsData = [{
       palette_color3: '#FFFFFF',
       palette_color4: '#FF45F0',
       palette_color5: '#FFFFFF',
-      project_id: 1
+      project_id: 2
     }
   ]
 }
 ]
 
+const createProject = (knex, project) => {
+  return knex('projects').insert({
+    project_Name: project.project_Name,
+    id: project.id,
+  })
+  .then(() => {
+    let palettePromises = [];
+
+    project.palettes.forEach(palette => {
+      palettePromises.push(createPalette(knex, palette))
+    })
+
+    return Promise.all(palettePromises);
+  })
+}
+
+const createPalette = (knex, palette) => {
+  return knex('palettes').insert(palette);
+}
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('palettes').del()
     .then(() => knex('projects').del(()))
-    .then(function () {
+    .then(() => {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      let projectPromises = [];
 
-exports.seed = function(knex, Promise) {
-  // We must return a Promise from within our seed function
-  // Without this initial `return` statement, the seed execution
-  // will end before the asynchronous tasks have completed
-  return knex('palettes').del() // delete all footnotes first
-    .then(() => knex('papers').del()) // delete all papers
-
-    // Now that we have a clean slate, we can re-insert our paper data
-    .then(() => {
-      return Promise.all([
-
-        // Insert a single paper, return the paper ID, insert 2 footnotes
-        knex('papers').insert({
-          title: 'Fooo', author: 'Bob', publisher: 'Minnesota'
-        }, 'id')
-        .then(paper => {
-          return knex('footnotes').insert([
-            { note: 'Lorem', paper_id: paper[0] },
-            { note: 'Dolor', paper_id: paper[0] }
-          ])
-        })
-        .then(() => console.log('Seeding complete!'))
-        .catch(error => console.log(`Error seeding data: ${error}`))
-      ]) // end return Promise.all
-    })
-    .catch(error => console.log(`Error seeding data: ${error}`));
-};
-
-const createPaper = (knex, paper) => {
-  return knex('papers').insert({
-    title: paper.title,
-    author: paper.author
-  }, 'id')
-  .then(paperId => {
-    let footnotePromises = [];
-
-    paper.footnotes.forEach(footnote => {
-      footnotePromises.push(
-        createFootnote(knex, {
-          note: footnote,
-          paper_id: paperId[0]
-        })
-      )
-    });
-
-    return Promise.all(footnotePromises);
-  })
-};
-
-const createFootnote = (knex, footnote) => {
-  return knex('footnotes').insert(footnote);
-};
-
-exports.seed = (knex, Promise) => {
-  return knex('footnotes').del() // delete footnotes first
-    .then(() => knex('papers').del()) // delete all papers
-    .then(() => {
-      let paperPromises = [];
-
-      papersData.forEach(paper => {
-        paperPromises.push(createPaper(knex, paper));
+      projectsData.forEach(project => {
+        projectPromises.push(createProject(knex, project))
       });
 
-      return Promise.all(paperPromises);
+      return Promise.all(projectPromises)
     })
-    .catch(error => console.log(`Error seeding data: ${error}`));
+    .then(() => console.log('Seeding is complete'))
+    .catch(error => console.log(`Error seeding data: ${error}`))
 };
