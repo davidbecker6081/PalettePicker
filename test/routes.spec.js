1e3de933da49e6ae526bfc5be62855fee73d433b
David Becker <david@beckersite.com>
2017-10-05 17:10:35 -0600

    Test for happy path for GET request to projects

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -41,4 +41,22 @@ describe('Client Routes', () => {
 
 describe('API Routes', () => {
 
+  describe('GET /api/projects', () => {
+    it('should return all of the projects', (done) => {
+      chai.request(server)
+      .get('/api/projects')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(2);
+        response.body.filter((project) => project.project_Name === 'Project1').length.should.equal(1);
+        response.body[0].should.have.property('project_Name');
+        // response.body[0].project_Name.should.equal('Project1');
+        response.body[0].should.have.property('id');
+        response.body.filter((project) => project.id === 1).length.should.equal(1);
+        done();
+      })
+    })
+  })
 });
